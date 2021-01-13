bf9ff67b0869a899f0d0bdd7dad6af20432ec309
LeeDeshuai <fortescarlet@163.com>
2021-01-13 09:37:40 +0800

    init

---
+++ ir.cpp
@@ -0,0 +1,100 @@
+#include "pxt.h"
+#include <map>
+#include <vector>
+#include "ReceiverIR.h"
+using namespace pxt;
+typedef vector<Action> vA;
+
+enum class Pins{
+  P0=  3,
+  P1=  2,
+  P2=  1,
+  P3=  4,
+  P4=  5,
+  P5=  17,
+  P6=  12,
+  P7=  11,
+  P8=  18,
+  P9=  10,
+  P10= 6,
+  P11= 26,
+  P12= 20,
+  P13= 23,
+  P14= 22,
+  P15= 21,
+  P16= 16,
+  P19= 0,
+  P20= 30
+};
+
+enum class RemoteButton {	
+	  PLAY = 0x38C7,
+	  NUM_200 = 0x18E7,
+	  NEXT = 0x10EF,
+	  Minus = 0x5AA5,
+	  Add = 0xB04F,
+	  EQ = 0x6897,
+	  NUM0 = 0x9867,
+	  NUM1 = 0xA25D,
+	  NUM2 = 0x629D,
+	  NUM3 = 0xE21D,
+	  NUM4 = 0xE01F,
+	  NUM5 = 0x02FD,
+	  NUM6 = 0xC23D,
+	  NUM7 = 0xE01F,
+	  NUM8 = 0xA857,
+	  NUM9 = 0x906F,
+};
+
+//% color=50 weight=19
+//% icon="\uf1eb"
+namespace Mbit_IR { 
+  map<RemoteButton, vA> actions;
+  map<RemoteButton, uint32_t> lastact;
+  Timer tsb; 
+  uint8_t buf[32];
+  uint32_t now;
+  ReceiverIR *rx;
+  RemoteIR::Format fmt = RemoteIR::UNKNOWN;
+
+  /**
+  * button pushed.
+  */
+  //% blockId=ir_received_left_event
+  //% block="on |%btn| button pressed"
+  void onPressEvent(RemoteButton btn, Action body) {
+    //if(actions.find(btn) == actions.end()) actions[btn] = new vector();
+    actions[btn].push_back(body);
+  }
+
+  void cA(vA runner){for(int i=0;i<runner.size();i++){runAction0(runner[i]);} }
+
+  void onReceivable(){
+    int x = rx->getData(&fmt, buf, 32 * 8);
+    if(actions.find((RemoteButton)buf[2]) == actions.end()) return;
+    now = tsb.read_ms();
+    if(now - lastact[(RemoteButton)buf[2]] < 100) return;
+    lastact[(RemoteButton)buf[2]] = now;
+    cA(actions[(RemoteButton)buf[2]]); 
+  }
+
+  void monitorIR(){
+    while(1){
+      while(rx->getState() != ReceiverIR::Received){ 
+        uBit.sleep(50);
+      }
+      onReceivable();
+    }
+  }
+
+  /**
+  * initialises local variablesssss
+  */
+  //% blockId=ir_init
+  //% block="connect ir receiver to %pin"
+  void init(Pins pin){
+    rx = new ReceiverIR((PinName)pin);
+    tsb.start(); //interrupt timer for debounce
+    create_fiber(monitorIR);
+  }
+}
